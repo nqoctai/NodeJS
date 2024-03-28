3e72d50db542dc8488a01badef1dadeb1410d748
nqoctai <jeweltai1210@gmail.com>
2024-03-28 20:30:59 +0700

    video23

--- src/server.js
+++ src/server.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+// config template engine
+app.set('views','./src/views/')
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/hoidanit', (req, res) => {
+    // res.send('Hello World!')
+    res.render('sample.ejs')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

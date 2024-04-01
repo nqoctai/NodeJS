840f1598108aa505c10c93733c434fed9d79bd05
nqoctai <jeweltai1210@gmail.com>
2024-04-01 09:35:55 +0700

    video37

--- src/server.js
+++ src/server.js
@@ -6,28 +6,20 @@ const app = express()
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOSTNAME;
 const webRoutes = require('./routes/web')
-const mysql = require('mysql2');
+const connection = require('./config/database')
 
 
 app.use('/', webRoutes);
 // config template engine
 configViewEngine(app);
 
-// test connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'hoidanit',
-  port: 3307,
-  password: '123456'
- });
+
 
  // simle query
  connection.query(
   'SELECT * FROM Users u;',
   function(err, results, fields) {
     console.log(">>>results = ",results); // results contains rows returned by server
-    console.log(">>> fields ",fields); // fields contains extra meta data about results, if available
   })
  
 
